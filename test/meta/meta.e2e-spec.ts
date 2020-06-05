293cf02ad51da4b6fb6c7059fa46069d6ede0c78
Tim Garthwaite <tim@hydrow.com>
2020-06-05 12:03:26 -0400

    Switch to supertest

--- test/meta/meta.e2e-spec.ts
+++ test/meta/meta.e2e-spec.ts
@@ -1,104 +1,70 @@
 import * as http from 'http';
-import { HttpStatus } from '@nestjs/common';
+import * as superTest from 'supertest';
+
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { AppModule } from '../../src/app.module';
+import { getAppOptions } from '../../src/init';
 
 describe('Meta controller', () => {
 
+    let module: TestingModule;
+    let app: INestApplication;
+
+    const request = (): superTest.SuperTest<superTest.Test> => superTest(app.getHttpServer());
+
+    beforeAll(async () => {
+        module = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+        app = module.createNestApplication(
+            undefined,
+            getAppOptions(),
+        );
+        await app.init();
+    });
+
+    afterAll(() => app.close());
+
     describe('GET /meta/fail', () => {
-        it('returns 500 INTERNAL SERVER ERROR', done => {
-            const data = JSON.stringify({value: 'Pete'});
-            const options = {
-                headers: {
-                    'X-Correlation-Id': 'GET /meta/fail test',
-                },
-                hostname: 'localhost',
-                method: 'GET',
-                path: '/meta/fail',
-                port: 3000,
-            }
-            const req = http.request(options, res => {
-                console.log(`statusCode: ${res.statusCode}`);
-                res.on('data', d => {
-                    process.stdout.write(d);
-                });
-                res.on('end', () => {
-                    expect(res.statusCode).toBe(HttpStatus.INTERNAL_SERVER_ERROR);
-                    done();
-                });
-            });
-            req.on('error', err => {
-                done(new Error('Hmm... how did this happen?'));
-            });
-            req.end();
+        it('returns 500 INTERNAL SERVER ERROR', async () => {
+            await request()
+                .get('/meta/fail')
+                .set('X-Correlation-Id', 'GET /meta/fail test')
+                .expect(HttpStatus.INTERNAL_SERVER_ERROR);
         });
     });
 
     describe('POST /meta/echo', () => {
 
         describe('a valid POST', () => {
-            it('returns 201 CREATED', done => {
-                const data = JSON.stringify({ value: 'Pete' });
-                const options = {
-                    headers: {
-                        'Content-Length': data.length,
-                        'Content-Type': 'application/json',
-                        'X-Correlation-Id': 'POST /meta/echo success case',
-                    },
-                    hostname: 'localhost',
-                    method: 'POST',
-                    path: '/meta/echo',
-                    port: 3000,
-                }
-                const req = http.request(options, res => {
-                    console.log(`statusCode: ${res.statusCode}`);
-                    res.on('data', d => {
-                        process.stdout.write(d);
-                    });
-                    res.on('end', () => {
-                        expect(res.statusCode).toBe(HttpStatus.CREATED);
-                        done();
-                    });
-                });
-                req.on('error', err => {
-                    done(err);
-                });
-                req.write(data);
-                req.end();
+            it('returns 201 CREATED', async () => {
+                const value = 'Pete';
+                const res = await request()
+                    .post('/meta/echo')
+                    .set('X-Correlation-Id', 'POST /meta/echo success case')
+                    .send({ value })
+                    .expect(HttpStatus.CREATED);
+                expect(res.body).toEqual({ pong: true, text: value });
             });
         });
 
         describe('an invalid POST that times out', () => {
-            it('returns 400 BAD REQUEST', done => {
-                const data = JSON.stringify({ value: 'Pete' });
-                const options = {
-                    hostname: 'localhost',
-                    port: 3000,
-                    path: '/meta/echo',
-                    method: 'POST',
-                    headers: {
-                        // Force it to expect an extra byte
-                        'Content-Length': data.length + 1,
-                        'Content-Type': 'application/json',
-                        'X-Correlation-Id': 'POST /meta/echo failure case',
-                    },
-                }
-                const req = http.request(options, res => {
-                    console.log(`statusCode: ${res.statusCode}`);
-                    res.on('data', d => {
-                        process.stdout.write(d);
-                    });
-                    res.on('end', () => {
-                        expect(res.statusCode).toBe(HttpStatus.BAD_REQUEST);
-                        done();
-                    });
-                });
-                req.on('error', err => {
-                    done(err);
-                });
-                req.write(data);
-                setTimeout(() => {
-                    req.abort();
-                    req.end();
-                }, 100);
+            it('returns 400 BAD REQUEST', async () => {
+                const value = 'Pete';
+                const data = { value };
+                const req = request()
+                    .post('/meta/echo')
+                    // Force it to expect an extra byte
+                    .set('Content-Length', (JSON.stringify(data).length + 1).toString())
+                    .set('X-Correlation-Id', 'POST /meta/echo failure case')
+                    .send(data);
+                await new Promise(resolve => setTimeout(resolve, 100));
+                req.abort();
+                req.end();
+                const res = await req;
+                expect(res.status).toBe(HttpStatus.BAD_REQUEST);
             });
         });
     });
